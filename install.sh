122a386d42e33afe90c6996b6368e5d5043ca20b
teeseng <andydomokun@gmail.com>
2016-09-03 20:10:16 -0500

    Install script added

--- install.sh
+++ install.sh
@@ -2,8 +2,22 @@ clear
 
 echo "Install vim setup"
 
+rm ~/.vimrc
+rm -rf ~/.vim
+
 # hard link .vimrc to vimrc file in repo
 ln $PWD/vimrc ~/.vimrc
 
+mkdir ~/.vim
+cp -a $PWD/vim ~/.vim
 
+mv ~/.vim/vim ~/
+rm -rf ~/.vim
+mv ~/vim ~/.vim
 
+cd ~/.vim/plugged/
+git clone https://github.com/scrooloose/syntastic.git
+
+git clone https://github.com/scrooloose/nerdtree.git
+
+git clone https://github.com/vim-airline/vim-airline.git
